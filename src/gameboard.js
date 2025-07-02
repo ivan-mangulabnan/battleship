bf4682b5b7fcb17148d90678eaa1f926e7ed36f1
ivan <ivanmangulabnan19@gmail.com>
2025-07-02 21:21:07 +0800

    Feat <gameboard.js>: Add placeShip method

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,80 +1,22 @@
 export class Gameboard {
   constructor () {
-    this.shipNodes = [];
-    this.missedShots = [];
-    this.hitShots = [];
+    this.coordsToShip = new Map();
+    this.shipToCoords = new Map();
   }
 
   placeShip (ship, coordinates) {
-    const isShipPresent = this.isShipPresent(ship);
-    
-    if (isShipPresent) {
-      const targetShip = this.getShipNode(ship);
-      targetShip.locations.push(coordinates);
-      return;
-    }
-
-    const newShipNode = this.createShipNode(ship);
-    newShipNode.locations.push(coordinates);
-    this.addShipNode(newShipNode);
+    this.updateShipLocations(ship, coordinates);
+    this.updateCoordinates(ship, coordinates);
   }
 
-  createShipNode (ship) {
-    return new ShipNode(ship);
+  updateShipLocations (ship, coords) {
+    const locations = this.shipToCoords.get(ship) || new Set();
+    locations.add(coords);
+
+    this.shipToCoords.set(ship, locations);
   }
 
-  addShipNode (shipNode) {
-    this.shipNodes.push(shipNode);
-  }
-
-  isShipPresent (ship) {
-    return this.shipNodes.some(shipNode => shipNode.ship === ship);
-  }
-
-  getShipNode (ship) {
-    return this.shipNodes.find(shipNode => shipNode.ship === ship);
-  }
-
-  receiveAttack (coordinates) {
-    const targetShipNode = this.getHitShip(coordinates);
-
-    if (targetShipNode) this.updateHitShots(targetShipNode, coordinates);
-    else this.updateMissedShots(coordinates);
-  }
-
-  getHitShip (coordinates) {
-    let targetShip;
-
-    for (const shipNode of this.shipNodes) {
-      for (const location of shipNode.locations) {
-        if (JSON.stringify(location) === JSON.stringify(coordinates)) {
-          targetShip = shipNode;
-          break;
-        }
-      }
-    }
-
-    return targetShip ? targetShip : null;
-  }
-
-  updateHitShots (shipNode, coordinates) {
-    const ship = shipNode.ship;
-    this.hitShots.push(coordinates);
-    ship.hit();
-  }
-
-  updateMissedShots (coordinates) {
-    this.missedShots.push(coordinates);
-  }
-
-  areAllShipsSunk () {
-    return this.shipNodes.every(shipNode => shipNode.ship.isSunk());
-  }
-}
-
-export class ShipNode {
-  constructor (ship) {
-    this.ship = ship;
-    this.locations = [];
+  updateCoordinates (ship, coords) {
+    this.coordsToShip.set(coords, ship);
   }
 }
