209458d2efb71d8a667b93bb1dccc947bceae6ce
ivan <ivanmangulabnan19@gmail.com>
2025-07-03 19:28:14 +0800

    Test <player.test.js>: getShipLocations method

--- src/player.test.js
+++ src/player.test.js
@@ -68,3 +68,68 @@ describe ('placeShipsRandomly method', () => {
     expect(coordinates.size).toBe(9);
   })
 })
+
+describe ('getShipLocations method', () => {
+  let player;
+  let ship1;
+  let ship2;
+  let ship3;
+  let location1;
+  let location2;
+  let location3;
+
+  beforeEach(() => {
+    player = new Player();
+    ship1 = player.ships[0];
+    ship2 = player.ships[1];
+    ship3 = player.ships[2];
+    location1 = ['00', '01', '02'];
+    location2 = ['20', '21', '22'];
+    location3 = ['40', '41', '42'];
+    player.board.moveShip(ship1, location1);
+    player.board.moveShip(ship2, location2);
+    player.board.moveShip(ship3, location3);
+  })
+
+  test ('getShipLocations returns array of arrays', () => {
+    const shipLocs = player.getShipLocations();
+    expect(Array.isArray(shipLocs)).toBeTruthy();
+
+    for (const locArr of shipLocs) {
+      expect(Array.isArray(locArr)).toBeTruthy();
+    }
+  })
+
+  test ('getShipLocations returns an array with length of exactly 3', () => {
+    const shipLocs = player.getShipLocations();
+    expect(shipLocs.length).toBe(3);
+  })
+
+  test ('each array inside array have length of exactly 3', () => {
+    const shipLocs = player.getShipLocations();
+    for (const arr of shipLocs) {
+      expect(arr.length).toBe(3);
+    }
+  })
+
+  test ('each array inside array have strings with the exact length of 2', () => {
+    const shipLocs = player.getShipLocations();
+    for (const arr of shipLocs) {
+      for (const loc of arr) {
+        expect(typeof loc).toBe('string');
+        expect(loc.length).toBe(2);
+
+        for (const char of loc) {
+          expect(typeof Number(char)).toBe('number');
+        }
+      }
+    }
+  })
+
+  test ('getShipLocations returns values in order', () => {
+    const shipLocs = player.getShipLocations();
+    expect(shipLocs[0]).toEqual(location1);
+    expect(shipLocs[1]).toEqual(location2);
+    expect(shipLocs[2]).toEqual(location3);
+  })
+})
