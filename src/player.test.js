fbb58aab0c2185c02ea4fb32213df92b06d0e081
ivan <ivanmangulabnan19@gmail.com>
2025-07-03 12:33:17 +0800

    Test <player.test.js>: Player class' ships property

--- src/player.test.js
+++ src/player.test.js
@@ -2,6 +2,18 @@ import { Player } from "./player.js";
 import { Gameboard } from "./gameboard.js";
 import { Ship } from "./ship.js";
 
+describe ('Player class', () => {
+  let player;
+
+  beforeEach(() => {
+    player = new Player();
+  })
+
+  test ('ships property should be instances of Ship class', () => {
+    player.ships.forEach(ship => expect(ship).toBeInstanceOf(Ship));
+  })
+})
+
 describe ('createShips method', () => {
   let player;
 
