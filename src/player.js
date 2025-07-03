9ee79f2b05a9cb79323b0b1d754c2b9720b4cf52
ivan <ivanmangulabnan19@gmail.com>
2025-07-03 12:23:02 +0800

    Feat <player.js>: add placeShipsRandomly method

--- src/player.js
+++ src/player.js
@@ -5,6 +5,7 @@ class Player {
   constructor () {
     this.board = new Gameboard();
     this.ships = this.createShips();
+    this.placeShipsRandomly();
   }
 
   createShips () {
@@ -17,6 +18,15 @@ class Player {
 
     return shipsArr;
   }
+
+  placeShipsRandomly () { // set for now. will make it random in cleanUp.
+    for (let i = 0; i < 3; i++) {
+      this.board.placeShip(this.ships[0], `${9 - i}0`); // 90,80,70
+      this.board.placeShip(this.ships[1], `8${5 - i}`); // 85,84,83
+      this.board.placeShip(this.ships[2], `${5 - i}8`); // 58,48,38
+    }
+  }
+
 }
 
 export class RealPlayer extends Player {
