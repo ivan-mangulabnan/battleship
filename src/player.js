547060e5910688aeeb0ccf59ddd054b23ce139ab
ivan <ivanmangulabnan19@gmail.com>
2025-07-05 12:49:16 +0800

    Feat <player.js>: modify placeShipsRandomly method

--- src/player.js
+++ src/player.js
@@ -19,11 +19,89 @@ export class Player {
     return shipsArr;
   }
 
-  #placeShipsRandomly () { // set for now. will make it random in cleanUp.
-    for (let i = 0; i < 3; i++) {
-      this.board.placeShip(this.ships[0], `${9 - i}0`); // 90,80,70
-      this.board.placeShip(this.ships[1], `8${5 - i}`); // 85,84,83
-      this.board.placeShip(this.ships[2], `${5 - i}8`); // 58,48,38
+  #placeShipsRandomly () {
+    const random = Math.floor(Math.random() * 10) + 1;
+
+    switch (random) {
+      case 1:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${9 - i}0`);
+          this.board.placeShip(this.ships[1], `8${5 - i}`);
+          this.board.placeShip(this.ships[2], `${5 - i}8`);
+        }
+        break;
+
+      case 2:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${i}0`);
+          this.board.placeShip(this.ships[1], `0${2 + i}`);
+          this.board.placeShip(this.ships[2], `${2 + i}2`);
+        }
+        break;
+
+      case 3:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${4}${i}`);
+          this.board.placeShip(this.ships[1], `${6 + i}6`);
+          this.board.placeShip(this.ships[2], `${8}${7 - i}`);
+        }
+        break;
+
+      case 4:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${1 + i}9`);
+          this.board.placeShip(this.ships[1], `7${i}`);
+          this.board.placeShip(this.ships[2], `${5 - i}5`);
+        }
+        break;
+
+      case 5:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `2${7 - i}`);
+          this.board.placeShip(this.ships[1], `${6 + i}3`);
+          this.board.placeShip(this.ships[2], `0${i}`);
+        }
+        break;
+
+      case 6:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${3}${i + 5}`);
+          this.board.placeShip(this.ships[1], `${7 - i}7`);
+          this.board.placeShip(this.ships[2], `9${2 + i}`);
+        }
+        break;
+
+      case 7:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${i + 6}1`);
+          this.board.placeShip(this.ships[1], `${2}${i + 3}`);
+          this.board.placeShip(this.ships[2], `${5 - i}8`);
+        }
+        break;
+
+      case 8:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${i}9`);
+          this.board.placeShip(this.ships[1], `${4 + i}0`);
+          this.board.placeShip(this.ships[2], `7${7 - i}`);
+        }
+        break;
+
+      case 9:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${1 + i}1`);
+          this.board.placeShip(this.ships[1], `${8}${i + 4}`);
+          this.board.placeShip(this.ships[2], `${5}${5 - i}`);
+        }
+        break;
+
+      case 10:
+        for (let i = 0; i < 3; i++) {
+          this.board.placeShip(this.ships[0], `${6}${i + 2}`);
+          this.board.placeShip(this.ships[1], `${i + 2}8`);
+          this.board.placeShip(this.ships[2], `0${6 - i}`);
+        }
+        break;
     }
   }
 
@@ -41,10 +119,15 @@ export class Player {
 }
 
 export class RealPlayer extends Player {
-  constructor () {
+  constructor () { // shipLocations parameter
     super();
     this.type = 'player';
+    // this.updateLocations(shipLocations);
   }
+
+  // updateLocations (shipLocations) {
+
+  // }
 }
 
 export class ComputerPlayer extends Player {
