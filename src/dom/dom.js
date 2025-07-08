ab05684d178a8e3bafa606f008b1f6210d4d20cb
ivan <ivanmangulabnan19@gmail.com>
2025-07-08 11:17:40 +0800

    Feat <dom.js>: generate dynamic board

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,1 +1,23 @@
 import './dom.css';
+
+function createBoard (parentEle) {
+  const row = 10;
+  const col = 10;
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.dataset.rowCol = `${i}${j}`;
+      parentEle.appendChild(cell);
+    }
+  }
+}
+
+export function showBoard () {
+  const pOneBoard = document.querySelector('#pOneBoard');
+  const pTwoBoard = document.querySelector('#pTwoBoard');
+
+  createBoard(pOneBoard);
+  createBoard(pTwoBoard);
+}
