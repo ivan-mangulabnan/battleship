3db5ff6875c2919a23fbfe5008108a3c170d5d2b
ivan <ivanmangulabnan19@gmail.com>
2025-07-10 20:45:20 +0800

    Feat <repositioning.js>: refactor relocateSection

--- src/dom/relocation/repositioning.js
+++ src/dom/relocation/repositioning.js
@@ -1,80 +1,134 @@
-export function showPositionForm () {
-  const utils = document.querySelector('#utils');
-  utils.innerHTML = '';
-
+export function relocateSection () {
+  const util = document.querySelector('#utils');
+  util.innerHTML = '';
+  
   const form = document.createElement('form');
-  form.classList.add('reposition-form');
+  form.classList.add('relocate-form');
 
-  const selectDiv = document.createElement('div');
-  selectDiv.classList.add('ship-wrapper');
-  const shipList = document.createElement('select');
-  shipList.id = 'shipSelect';
-  const shipLabel = document.createElement('label');
-  shipLabel.htmlFor = 'shipSelect';
-  shipLabel.textContent = 'Select a ship to move';
+  const selectSection = selectShip();
+  const inputSection = shipInputs();
+  const btnSection = createBtn();
 
-  const createOptions = (value) => {
-    const option = document.createElement('option');
-    option.value = `${value}`;
-    option.text = `${value}`;
+  form.append(selectSection, inputSection, btnSection);
+  util.appendChild(form);
+}
 
-    return option;
+// Row Col Inputs Section below.
+
+function shipInputs () {  
+  const shipWrapper = document.createElement('div');
+  shipWrapper.classList.add('ship-inputs-wrapper');
+
+  const shipParts = [];
+  for (let i = 1; i <= 3; i++) {
+    shipParts.push(createRowColInput(`${i}`));
   }
 
-  const ship1 = createOptions('ship1');
-  const ship2 = createOptions('ship2');
-  const ship3 = createOptions('ship3');
-
-  shipList.append(ship1, ship2, ship3);
-  selectDiv.append(shipLabel, shipList);
-
-  const createShipPart = () => {
-    const div = document.createElement('div');
-    div.classList.add('ship-part');
-
-    const row = document.createElement('input');
-    row.classList.add('row');
-    const col = document.createElement('input');
-    col.classList.add('col');
-
-    const setRestrictions = (input) => {
-      input.type = 'text';
-      input.minLength = 1;
-      input.maxLength = 1;
-      input.pattern = '[0-9]';
-    }
-
-    setRestrictions(row);
-    setRestrictions(col);
-
-    div.append(row,col);
-    return div;
-  }
-  
-  const inputsWrapper = document.createElement('div');
-  inputsWrapper.classList.add('ship-part-wrapper');
-  
-  const shipPart1 = createShipPart();
-  const shipPart2 = createShipPart();
-  const shipPart3 = createShipPart();
-
-  inputsWrapper.append(shipPart1, shipPart2, shipPart3);
-
-  const btnDiv = document.createElement('div');
-
-  const createBtn = (text) => {
-    const btn = document.createElement('button');
-    btn.type = 'button';
-    btn.textContent = `${text}`;
-
-    return btn;
+  for (const shipPart of shipParts) {
+    shipWrapper.appendChild(shipPart);
   }
 
-  const discardBtn = createBtn('Discard');
-  const applyBtn = createBtn('Apply');
+  return shipWrapper;
+}
 
-  btnDiv.append(discardBtn, applyBtn);
+function createRowColInput (num) {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('row-col-wrapper');
 
-  form.append(selectDiv, inputsWrapper, btnDiv);
-  utils.appendChild(form);
+  const rowWrapper = document.createElement('div');
+  rowWrapper.classList.add('row-wrapper', 'inner-wrapper');
+
+  const colWrapper = document.createElement('div');
+  colWrapper.classList.add('col-wrapper', 'inner-wrapper');
+
+  const rowInput = document.createElement('input');
+  rowInput.id = `row${num}`;
+  const colInput = document.createElement('input');
+  colInput.id = `col${num}`;
+
+  const rowLabel = document.createElement('label');
+  rowLabel.htmlFor = rowInput.id;
+  rowLabel.textContent = 'row';
+
+  const colLabel = document.createElement('label');
+  colLabel.htmlFor = colInput.id;
+  colLabel.textContent = 'col';
+
+  setRestrictions([rowInput, colInput]);
+
+  rowWrapper.append(rowLabel, rowInput);
+  colWrapper.append(colLabel, colInput);
+  wrapper.append(rowWrapper, colWrapper);
+  return wrapper;
+}
+
+function setRestrictions (inputArr) {
+  for (const input of inputArr) {
+    input.type = 'text';
+    input.minLength = 1;
+    input.maxLength = 1;
+    input.pattern = '[0-9]';
+  }
+}
+
+// Select Ship Section below.
+
+function selectShip () {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('ship-wrapper');
+
+  const shipSelect = document.createElement('select');
+  shipSelect.id = 'shipSelect';
+
+  const label = document.createElement('label');
+  label.htmlFor = shipSelect.id;
+  label.textContent = 'Select ship to move';
+
+  const optionArr = createOptions(generateOption);
+  setOptions(shipSelect, optionArr);
+
+  wrapper.append(label, shipSelect);
+  return wrapper;
+}
+
+function setOptions (select, optionArr) {
+  for (const option of optionArr) {
+    select.appendChild(option);
+  }
+}
+
+function createOptions (callback) {
+  const SHIP_COUNT = 3;
+  const options = [];
+  for (let i = 1; i <= SHIP_COUNT; i++) {
+    options.push(callback(`ship${i}`));
+  }
+
+  return options;
+}
+
+function generateOption (value) {
+  const option = document.createElement('option');
+  option.value = `${value}`;
+  option.textContent = `${value}`;
+
+  return option;
+}
+
+// Buttons below.
+
+function createBtn () {
+  const btnWrapper = document.createElement('div');
+  btnWrapper.classList.add('btn-wrapper');
+
+  const disardBtn = document.createElement('button');
+  disardBtn.type = 'button';
+  disardBtn.textContent = 'DISCARD';
+
+  const applyBtn = document.createElement('button');
+  applyBtn.type = 'submit';
+  applyBtn.textContent = 'APPLY';
+
+  btnWrapper.append(disardBtn, applyBtn);
+  return btnWrapper;
 }
