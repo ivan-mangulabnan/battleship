c316176ab835dbded4f22e7a298251edd4fc40b5
ivan <ivanmangulabnan19@gmail.com>
2025-07-12 17:47:10 +0800

    Feat <index.js>: add eventListeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import './index.css';
-import { showBoard } from './dom/dom.js';
+import { changeDisplayCoords, dummyPlayer, initialize, randomize, showShipCoords, discardRepositioning, applyShipChange } from './dom/dom.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  showBoard();
+  initialize(dummyPlayer);
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.matches('#randomBtn')) {
+    randomize();
+  }
+
+  if (event.target.matches('#moveShipBtn')) {
+    showShipCoords(dummyPlayer);
+  }
+
+  if (event.target.matches('#discardBtn')) {
+    discardRepositioning();
+  }
+})
+
+document.addEventListener('change', (event) => {
+  if (event.target.matches('#shipSelect')) {
+    changeDisplayCoords(dummyPlayer);
+  }
+})
+
+document.addEventListener('submit', (event) => {
+  if (event.target.matches('.relocate-form')) {
+    applyShipChange(dummyPlayer, event);
+  }
 })
